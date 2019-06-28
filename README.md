af6ddb9d80540c02e2bb137e1845216f75d103ac
Yeswanth <swamiyeswanth@gmail.com>
2019-06-28 12:22:51 +0530

    Added documentation

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # MigrateToAndroidX
 Python Script to automatically migrate imports to AndroidX 
+
+## What does it do? 
+* Uses the bindings file provided by [Android](https://developer.android.com/jetpack/androidx/migrate)
+* Looks at all the `node_modules` java files and searches for the old bindings (Usually of the form `android.support.*`) and replaces with the new AndroidX imports(Of the form `androidx.*`)
+* Currently, I'm only looking for a sublist of imports (that is used by most react-native libraries) and replacing them. 
+The sublist of imports are below:-
+1. `android.support.annotation.**`
+2. `android.support.annotation.RequiresPermission.**`
+3. `android.support.design.**` 
+4. `android.support.v4.**`
+
+## Usage
+```python3 migrateToAndroidX.py --node_modules ~/projects/rnProject/node_modules/```
+
--- migrateToAndroidX.py
+++ migrateToAndroidX.py
@@ -1,6 +1,7 @@
 import csv
 import glob
 import os
+import argparse
 cwd = os.getcwd()
 
 csvfile_path = cwd + "/androidx-class-mapping.csv"
@@ -11,7 +12,13 @@ to_include_replacement_patterns = [
  "android.support.design",
  "android.support.v4",
 ] 
-search_string = cwd + "/node_modules/**/src/main/java/**/*.java"
+search_string = "**/src/main/java/**/*.java"
+
+"""
+1. Looks at the csv file 
+2. Looks for the patterns
+3. Returns a dictionary of old import and new import
+"""
 def get_replacements():
     replacements = {} 
     for row in csv.reader(csvfile):
@@ -21,10 +28,16 @@ def get_replacements():
     return replacements
 
 
-if __name__ == '__main__':
+"""
+1. Looks for all the files that fall into the pattern
+2. It searches for the old imports in each of the file and replaces 
+   with the new imports 
+3. Saves the files after replacing
+"""
+def search_files(file_path):
     i = 0
     replacements = get_replacements()
-    file_names = glob.glob(search_string,recursive=True)
+    file_names = glob.glob(file_path + search_string,recursive=True)
     for file_name in file_names:
         file_data = open(file_name, 'r').read()
         for k,v in replacements.items():
@@ -33,7 +46,13 @@ if __name__ == '__main__':
                 print(file_name,k,v)
                 file_data = file_data.replace(k,v)
                 open(file_name, 'w').write(file_data)
-                print("=======",i+1)
+                print("== Migrated ",i+1, " files to AndroidX imports ==\n")
 
-           
-            
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Migrate your React-Native app to use AndroidX')
+    parser.add_argument('--node_modules', dest='node_modules_path', help='Node Modules folder location')
+    args = parser.parse_args()
+    if args.node_modules_path:
+        search_files(args.node_modules_path)
+    else:
+        print("Invalid arguments. Use -h to see help on how to use this script")
